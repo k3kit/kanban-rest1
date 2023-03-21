22aed8844e496538e1df27f19fa62b68c363f0f5
k3kit <hadouken7809@gmail.com>
2023-03-21 15:43:59 +0400

    init

---
+++ src/ormconfig.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import path from 'path';
+import { ConnectionOptions } from 'typeorm';
+
+dotenv.config({
+  path: path.join(__dirname, '.env'),
+});
+
+const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT, POSTGRES_HOST } = process.env;
+const LOCAL_URL = `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`;
+const RENDER_URL = `postgres://admin:LEJVfScdy1XnCtSsyRnrDbnFK4j0pGQV@dpg-cfv5lql3t39doaoguha0-a.frankfurt-postgres.render.com:5432/kanban_ti6g`;
+
+export default {
+  type: 'postgres',
+  cache: false,
+  url: (process.env.DATABASE_URL as string) || RENDER_URL,
+  synchronize: false,
+  logging: false,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  entities: ['src/resources/**/**.entity{.ts,.js}'],
+  migrations: ['./migrations/*.ts'],
+} as ConnectionOptions;
